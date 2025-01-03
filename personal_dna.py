3dfeefa22c11790a88e70e56fc68e2707d6e4616
Darien <galleon_caboose_0r@icloud.com>
2025-01-03 11:38:55 -0700

    Commit 1: Project Definitions
    Including project information, project steps

---
+++ personal_dna.py
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------------
+#
+# PROJECT INFORMATION
+# Author: Darien Prall
+# Start Date: 01-03-2025 (mmddyyyy)
+# End Date: 
+# Project Description: A personal project to further understand Python and Genomic Analysis
+#
+# PROJECT STEPS
+# Task 1: Count Allele Frequencies
+# Task 2: Group RSIDs by Chromosome
+# Task 3: Filter RSIDs by Chromosome and Position Range
+# Task 4: Visualize Chromosome SNP Distribution
+# Task 5: Lookup RSID Information from a Database
+# Task 6: Identify Homozygous and Heterozygous SNPs
+# Task 7: Identify Disease
+# Task 8: Final Report
+#
+#----------------------------------------------------------------------
